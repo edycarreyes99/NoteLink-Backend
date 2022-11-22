6d161743380385a05845350ce024e34335e8d47e
Edycar Reyes <edycarreyes@gmail.com>
2022-11-21 19:06:50 -0600

    Generated the files for the 'Auth' Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './modules/notes/notes.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [NotesModule],
+  imports: [NotesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
