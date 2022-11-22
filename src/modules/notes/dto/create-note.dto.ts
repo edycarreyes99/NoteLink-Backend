fe765ad5b46b9bf66fbc7b4190309052d8ef9cc7
Edycar Reyes <edycarreyes@gmail.com>
2022-11-22 15:31:39 -0600

    Defined the DTOs for a Note

--- src/modules/notes/dto/create-note.dto.ts
+++ src/modules/notes/dto/create-note.dto.ts
@@ -1,1 +1,23 @@
-export class CreateNoteDto {}
+import {
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  IsUrl,
+  MinLength,
+} from 'class-validator';
+
+export class CreateNoteDto {
+  @IsNotEmpty()
+  @MinLength(3)
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @MinLength(3)
+  @IsString()
+  description: string;
+
+  @IsArray()
+  @IsUrl({ each: true })
+  images?: string[];
+}
