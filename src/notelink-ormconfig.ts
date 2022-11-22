e055cde9aa3de92b4513ae8bf6619a7ac0ca5c90
Edycar Reyes <edycarreyes@gmail.com>
2022-11-22 02:05:29 -0600

    Created the database configuration file for the TypeORM.

---
+++ src/notelink-ormconfig.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+dotenv.config();
+
+const config: PostgresConnectionOptions = {
+  name: 'notelink',
+  type: 'postgres',
+  schema: 'public',
+  host: process.env.NOTELINK_DB_HOST,
+  port: parseInt(process.env.NOTELINK_DB_PORT, 10),
+  username: process.env.NOTELINK_DB_USER,
+  password: process.env.NOTELINK_DB_PASSWORD,
+  database: process.env.NOTELINK_DB_NAME,
+  entities: [],
+  synchronize: true,
+  migrationsRun: true,
+  logging: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  ssl: false,
+};
+
+export default new DataSource(config);
