09523e64cb830e0c85d461f97f36c5c14ce0802c
Edycar Reyes <edycarreyes@gmail.com>
2022-11-22 15:43:59 -0600

    generated the validations to create a new note in the notes.controller file

--- src/modules/notes/controllers/notes.controller.ts
+++ src/modules/notes/controllers/notes.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+} from '@nestjs/common';
 import { NotesService } from '../services/notes.service';
 import { CreateNoteDto } from '../dto/create-note.dto';
 import { UpdateNoteDto } from '../dto/update-note.dto';
@@ -9,6 +18,31 @@ export class NotesController {
 
   @Post()
   create(@Body() createNoteDto: CreateNoteDto) {
+    if (!createNoteDto) {
+      throw new BadRequestException('Please provide a valid note');
+    }
+
+    // Validate if the name of the note is not null and not empty and if it is longer than 3 characters
+    if (!createNoteDto.name || createNoteDto.name.trim() === '') {
+      return new BadRequestException('Name is required');
+    } else if (createNoteDto.name.trim().length < 3) {
+      return new BadRequestException('Name must be at least 3 characters long');
+    }
+
+    // Validate if the description of the note is not null and not empty and if it is longer than 3 characters
+    if (!createNoteDto.description || createNoteDto.description.trim() === '') {
+      return new BadRequestException('Description is required');
+    } else if (createNoteDto.description.trim().length < 3) {
+      return new BadRequestException(
+        'Description must be at least 3 characters long',
+      );
+    }
+
+    // Validate if the images array is null to initialize it as an empty array
+    if (!createNoteDto.images) {
+      createNoteDto.images = [];
+    }
+
     return this.notesService.create(createNoteDto);
   }
 
