abf284d0035b9791594db1d2a1b0a5c1f09038a2
Edycar Reyes <edycarreyes@gmail.com>
2022-11-24 01:45:00 -0600

    added and enabled the 'helmet' dependency for the API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api');
+  app.use(helmet());
   app.enableCors();
   await app.listen(3000);
 }
